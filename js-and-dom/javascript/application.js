c747e6456e9c9278ff3cce142cb0ad1f931c4629
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-06-01 11:04:01 +0900

    done

--- js-and-dom/javascript/application.js
+++ js-and-dom/javascript/application.js
@@ -1,1 +1,31 @@
-console.log("Hello from JavaScript!");
+const players = document.querySelector("ul#players");
+
+players.insertAdjacentHTML(
+  "beforeend",
+  "<li>Yoko</li><li>David</li><li>Andrew</li>"
+);
+
+const john = players.querySelector(".leader");
+
+const countries = document.querySelectorAll("ol#fifa-wins > li");
+
+// France (2 wins)
+const winners = document.querySelector("ol#fifa-wins");
+
+winners.insertAdjacentHTML("beforeend", "<li>France (2 wins)</li>");
+
+winners.style.backgroundColor = "yellow";
+
+winners.classList.toggle("red");
+
+const emailField = document.querySelector("#email");
+
+console.log(emailField.value);
+
+const home = document.querySelector("#home");
+
+console.log(home.innerText);
+console.log(home.innerHTML);
+console.log(home.attributes.href.value);
+
+home.innerHTML = "Le Wagon <strong>Rocks!!!</strong>";
